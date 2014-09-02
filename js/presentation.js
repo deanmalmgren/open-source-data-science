c1c4ec0c0e52e0fc86dd1c66c0e3a38eccc854b5
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-09-02 05:30:18 -0500

    first pass at using handlebars.js for rendering content from json

--- js/presentation.js
+++ js/presentation.js
@@ -8,6 +8,55 @@
 	document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
     }
 
-    // instantiate the impress api
-    var api = impress().init();
+    // fill the content with an ajax call
+    $.ajax({
+	url: "tools.json",
+	dataType: "json",
+	cache: false,
+    }).done(function (jqXHR, status, error) {
+
+	// render the content with handlebars
+	render_detail_content(jqXHR);
+
+	// instantiate the impress api
+	var api = impress().init();
+
+    }).fail(function (jqXHR, status, error) {
+	console.log("FAIL", jqXHR);
+	console.log("FAIL", status);
+	console.log("FAIL", error);
+    })
+
 })()
+
+
+function render_detail_content(json) {
+
+    // compile the handlebars template
+    var template_source = $("#detail-template").html();
+    var template = Handlebars.compile(template_source);
+
+    // for each details slide, add the content using the compiled template
+    $(".details").each(function (i, element) {
+
+	// find all tools that match this element id
+	var element_id = $(element).attr('id');
+	var tools = json.filter(function (tool) {
+	    return tool['good for'].some(function (good_for) {
+		return good_for === element_id;
+	    });
+	});
+
+	// render the template
+	$(element).html(template({
+	    detail_title: get_detail_title(element_id),
+	    packages: tools,
+	}));
+
+    });
+}
+
+
+function get_detail_title(element_id) {
+    return element_id.split('-')[1].replace('_', ' ');
+}
